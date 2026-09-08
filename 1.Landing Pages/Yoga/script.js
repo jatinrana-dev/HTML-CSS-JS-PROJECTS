4944ad88d072ad5d4f37dd429ac9c5da519fe540
Jatin Rana <jatinrana.code@gmail.com>
2026-09-08 11:13:10 +0530

    Project is Completed

--- 1.Landing Pages/Yoga/script.js
+++ 1.Landing Pages/Yoga/script.js
@@ -20,3 +20,37 @@ navlinks.addEventListener("click", ()=> {
     menubtnicon.setAttribute("class", "ri-menu-4-line")
 }
 )
+
+
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header-image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".header-content h2", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header-content h1", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".header-content p", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".header__btn", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+ScrollReveal().reveal(".header__socials li", {
+  ...scrollRevealOption,
+  delay: 2500,
+  interval: 500,
+});
