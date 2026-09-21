d23fb4472ec02d0df5fdab73d6c1abbccef05e7a
Jatin Rana <jatinrana.code@gmail.com>
2026-09-21 23:56:21 +0530

    section-5-top is added

--- 1.Landing Pages/Lazarev-agency-clone/script.js
+++ 1.Landing Pages/Lazarev-agency-clone/script.js
@@ -27,22 +27,52 @@ video.addEventListener("click",function(){
 
 section3video()
 
-var videos = document.querySelectorAll("#sec5vidoes")
+var videos = document.querySelectorAll("#section5top")
 
 
 videos.forEach(function(elem){
-elem.addEventListener("mouseenter",function(){
-    elem.childNodes[3].style.opacity = 1
-    elem.childNodes[3].play()
-})
+
+    var video = elem.querySelector('.video video')
+    elem.addEventListener('mouseenter',function(){
+        video.play()
+        video.style.opacity = 1;
+    })
+    elem.addEventListener('mouseleave',function(){
+        video.pause()
+        video.load()
+        video.style.opactiy = 0
+    })
 })
 
 
-videos.forEach(function(elem){
-elem.addEventListener("mouseleave",function(){
-    elem.childNodes[3].style.opacity = 0
-    elem.childNodes[3].load()
-})
+var videoss = document.querySelectorAll("#section5bottom")
+
+
+
+videoss.forEach(function(elem){
+
+    var videoo = elem.querySelector('.video video')
+    elem.addEventListener('mouseenter',function(){
+        videoo.play()
+        videoo.style.opacity = 1;
+    })
+    elem.addEventListener('mouseleave',function(){
+        videoo.pause()
+        videoo.load()
+        videoo.style.opacity = 0
+    })
 })
 
 
+
+
+
+const sectionTop = document.querySelector('.section-5-top');
+
+sectionTop.addEventListener('mouseenter', () => {
+    sectionTop.classList.add('active');
+});
+
+sectionTop.addEventListener('mouseleave', () => {
+    sectionTop.classList.remove('active');
+});
